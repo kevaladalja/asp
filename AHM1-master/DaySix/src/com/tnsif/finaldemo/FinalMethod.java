b4243d31dbd8dd7665faa9dc2b14262d1c0c0ef3
kevaladalja <135017514+kevaladalja@users.noreply.github.com>
2023-08-02 12:37:59 +0530

    Add files via upload

---
+++ AHM1-master/DaySix/src/com/tnsif/finaldemo/FinalMethod.java
@@ -0,0 +1,18 @@
+package com.tnsif.finaldemo;
+
+public static void main(String[] args) {
+	
+}
+
+final public class FinalMethod {
+	
+	
+	final int a=40;
+	
+	//final method
+	final void show()
+	{
+		System.out.println(a);
+	}
+
+}
